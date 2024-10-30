70dd6ecaebbc58d3dc04dbd79f27a3d326d5cd1b
JONATHAN ODOH <jonathanodoh3140@gmail.com>
2024-10-30 11:47:58 +0100

    Building methods handling database connection, disconnection, and status checking.

--- app/ZuriORM.php
+++ app/ZuriORM.php
@@ -7,5 +7,32 @@ use PDOException;
 
 class ZuriORM
 {
+    private static $connection;
+    private $statement;
 
+
+    public function __construct($host, $dbname, $username, $password)
+    {
+        $this->connect($host, $dbname, $username, $password);
+    }
+
+    public function connect($host, $dbname, $username, $password)
+    {
+        try {
+            self::$connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function closeConnection()
+    {
+        self::$connection = null;
+    }
+
+    public function getConnectionStatus()
+    {
+        return self::$connection ? "Connected" : "Not connected";
+    }
 }
