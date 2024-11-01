d6b07cef9dec82b309c400c9b6b6b202680444d9
JONATHAN ODOH <jonathanodoh3140@gmail.com>
2024-11-01 16:44:46 +0100

    Working CRUD operations, Create, Read, Update, and Delete

--- app/ZuriORM.php
+++ app/ZuriORM.php
@@ -35,4 +35,29 @@ class ZuriORM
     {
         return self::$connection ? "Connected" : "Not connected";
     }
+
+   
+    public function create($table, array $data)
+    {
+        $columns = implode(',', array_keys($data));
+        $placeholders = implode(',', array_map(fn($v) => ":$v", array_keys($data)));
+        $sql = "INSERT INTO $table ($columns) VALUES ($placeholders)";
+        $statement = self::$connection->prepare($sql);
+        $statement->execute($data);
+        return self::$connection->lastInsertId();
+    }
+    public function read($table, $conditions = [])
+    {
+        $where = $this->buildWhereClause($conditions);
+        $sql = "SELECT * FROM $table $where";
+        $statement = self::$connection->prepare($sql);
+        $statement->execute(array_values($conditions));
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // On finish work here (Clause)
+
+    private function buildWhereClause($conditions)
+    {
+    }
 }
