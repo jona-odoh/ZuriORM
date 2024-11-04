fbc4d59978fada0dd53662eb334677fa77ff5d1d
JONATHAN ODOH <jonathanodoh3140@gmail.com>
2024-11-04 15:37:01 +0100

    Working on database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
-
-
-
-?>
+return [
+    'host' => 'localhost',
+    'database' => 'my_database',
+    'username' => 'username',
+    'password' => 'password',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'driver' => 'mysql', // Could be 'pgsql', 'sqlite', etc.
+];
